9069abedd53033571eb61e69b71955389902d674
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-05-03 23:25:23 +0200

    added function

---
+++ src/fr_atol.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   fr_atol.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fras <fras@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/05/03 23:19:49 by fras          #+#    #+#                 */
+/*   Updated: 2023/05/03 23:25:03 by fras          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+long	fr_atol(const char *str)
+{
+	long	rtn;
+	int		i;
+	int		sign;
+
+	i = 0;
+	rtn = 0;
+	sign = 1;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		rtn *= 10;
+		rtn += str[i++] - '0';
+	}
+	return (rtn * sign);
+}
